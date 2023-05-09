0b6f2552615e2015e23ae9f49f9abc498ac3e325
Jimmy Yao <resp@eample.com>
2023-05-09 15:59:25 +0800

    merge from local

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,7 +4,7 @@ import { OpenAI } from "langchain/llms/openai";
 // @ts-ignore
 export async function load() {
   //create openai embeddings with api key 
-  const model = new OpenAI({ openAIApiKey: "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", temperature: 0.8 });
+  const model = new OpenAI();
   const res = await model.call(
     "What would be a good company name a company that makes colorful socks?"
   );
